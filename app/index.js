f61c3d720bc736b10c4fb7e3d5e72ca70eb4136c
Devin Warrick <dev.warr@gmail.com>
2019-12-12 01:26:57 -0600

    Add more methods to communicate with front-end

--- app/index.js
+++ app/index.js
@@ -1,21 +1,36 @@
+const matchDatabase = require("../data/match")
+
 const app = socket => {
     console.log("socket Connection")
     console.log(socket.id)
-    socket.emit("view", "CREATE_USER");
-
+    
     socket.on("viewChanged", () => {
         console.log("View Changed!")
     })
-
+    
     socket.on("createMatch", username => {
-        console.log()
-        console.log(`Match created with Socket ${socket.id} and username ${username}`)
+        const newMatch = matchDatabase.createMatch(username, socket.id)
+        if (newMatch) {
+            socket.emit("matchCreated")
+            socket.emit("matchInfo", newMatch)
+            socket.emit("view", "GAME_SETTINGS")
+        } else socket.emit("matchCreationFail")
+    })
+    
+    socket.on("joinMatch", matchLink => {
+        if (matchLink) {
+            const joinMatch = matchDatabase.findMatchByLink(matchLink)
+            socket.emit("matchInfo", joinMatch)
+        }
+        
+        socket.emit("view", "CREATE_USER")
     })
 
-    socket.on("joinMatch", username => {
-        console.log()
-        console.log(`Socket ${socket.id}, username ${username}, is looking to join!`)
+    socket.on("findAllMatches", () => {
+        const allMatches = matchDatabase.findAllMatches()
+        socket.emit("allMatchesReturn", allMatches)
     })
+
 }
 
 module.exports = app
